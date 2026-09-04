0dbe3f6660e3d0bc766f1c6e6ceb7683dc8183a5
Shabib Ahmed <shabibsahmed@gmail.com>
2026-09-04 11:44:39 -0400

    deploy scripts

--- AmbientDisplay/AppDelegate.m
+++ AmbientDisplay/AppDelegate.m
@@ -15,26 +15,22 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // 1. Create the UIWindow manually to span the entire iPhone 6 Retina display
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // 2. Load the initial ViewController from Main.storyboard
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    self.window.rootViewController = [storyboard instantiateInitialViewController];
+    [self.window makeKeyAndVisible];
+    
+    // 3. Keep the display awake indefinitely so the desk clock stays illuminated
+    [UIApplication sharedApplication].idleTimerDisabled = YES;
+    
     return YES;
 }
 
 
 #pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
-}
-
-
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
 
 
 @end
